f6d64f4f1a7ab14588d72eadc6d7723d2260ad00
Centaur <425540677@qq.com>
2023-08-22 00:44:37 +0800

    commit

--- thinking-in-spring/ioc-container-overview/src/main/java/org/centaur/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/centaur/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 import java.util.Map;
 
@@ -22,17 +23,21 @@ public class DependencyInjectionDemo {
 
     public static void main(String[] args) {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+        //依赖来源一：自定义的bean
         UserRepository userRepository = (UserRepository) beanFactory.getBean("userRepository");
 //        System.out.println(userRepository);
+        //依赖来源二：容器内建的依赖
         System.out.println(userRepository.getBeanFactory());
 //        System.out.println(userRepository.getBeanFactory() == beanFactory);
-
         ObjectFactory objectFactory = userRepository.getObjectFactory();
         // todo: 为什么会一样？
         System.out.println(objectFactory.getObject() == beanFactory);
 
 //        User user = userRepository.getUserObjectFactory().getObject();
 //        System.out.println(user);
+        //依赖来源三：容器内建的Bean
+        Environment environment = beanFactory.getBean(Environment.class);
+        System.out.println("容器内置的Bean：" + environment);
 
         //依赖查找（错误获取）
 //        System.out.println(beanFactory.getBean(BeanFactory.class));
