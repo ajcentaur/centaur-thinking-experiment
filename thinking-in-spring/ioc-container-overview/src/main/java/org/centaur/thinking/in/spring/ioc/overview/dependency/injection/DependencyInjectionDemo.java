6ee04184e267ea8b451777a3e0a8d7230d0725c1
CentaurLuo <luochengbo@sciecomm.cn>
2023-08-22 15:53:24 +0800

    commit

--- thinking-in-spring/ioc-container-overview/src/main/java/org/centaur/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/centaur/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -6,6 +6,7 @@ import org.centaur.thinking.in.spring.ioc.overview.domain.User;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.Environment;
 
@@ -22,25 +23,42 @@ import java.util.Map;
 public class DependencyInjectionDemo {
 
     public static void main(String[] args) {
+        // BeanFactory是Spring底层IoC容器
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+        //ApplicationContext是具备应用特性的BeanFactory的超集
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
         //依赖来源一：自定义的bean
         UserRepository userRepository = (UserRepository) beanFactory.getBean("userRepository");
 //        System.out.println(userRepository);
         //依赖来源二：容器内建的依赖
-        System.out.println(userRepository.getBeanFactory());
-//        System.out.println(userRepository.getBeanFactory() == beanFactory);
-        ObjectFactory objectFactory = userRepository.getObjectFactory();
+//        BeanFactory userBeanFactory = userRepository.getBeanFactory();
+//        System.out.println(userBeanFactory);
+//        System.out.println(userBeanFactory == beanFactory);
+//        ObjectFactory objectFactory = userRepository.getObjectFactory();
         // todo: 为什么会一样？
-        System.out.println(objectFactory.getObject() == beanFactory);
+//        System.out.println(objectFactory.getObject() == beanFactory);
 
 //        User user = userRepository.getUserObjectFactory().getObject();
 //        System.out.println(user);
         //依赖来源三：容器内建的Bean
-        Environment environment = beanFactory.getBean(Environment.class);
-        System.out.println("容器内置的Bean：" + environment);
+//        Environment environment = beanFactory.getBean(Environment.class);
+//        System.out.println("容器内置的Bean：" + environment);
 
         //依赖查找（错误获取）
 //        System.out.println(beanFactory.getBean(BeanFactory.class));
+
+        //BeanFactory和ApplicationContext谁才是Spring的容器？
+        whoIsSpringContainer(userRepository, beanFactory);
+    }
+
+    private static void whoIsSpringContainer(UserRepository userRepository, BeanFactory beanFactory){
+        // ConfigurableApplicationContext <- ApplicationContext <- BeanFactory
+        // ConfigurableApplicationContext#getBeanFactory()
+
+        System.out.println("UserRepository 中的BeanFactory: " + userRepository.getBeanFactory());
+        System.out.println("通过 ClassPathXmlApplicationContext 构造的中的BeanFactory: " + beanFactory);
+        System.out.println(userRepository.getBeanFactory() == beanFactory);
+
     }
 
 }
