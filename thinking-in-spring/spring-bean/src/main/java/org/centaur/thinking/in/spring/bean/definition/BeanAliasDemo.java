77e42bcfa66e2c78d144b982affe3c4d2508ed3e
Centaur <425540677@qq.com>
2023-08-23 23:36:08 +0800

    commit

---
+++ thinking-in-spring/spring-bean/src/main/java/org/centaur/thinking/in/spring/bean/definition/BeanAliasDemo.java
@@ -0,0 +1,20 @@
+package org.centaur.thinking.in.spring.bean.definition;
+
+import org.centaur.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Centaur
+ * @version 1.0
+ * @description: SpringBean 别名 Demo
+ * @date 2023/8/23 23:32
+ */
+public class BeanAliasDemo {
+    public static void main(String[] args) {
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-definition-context.xml");
+        User user = beanFactory.getBean("user", User.class);
+        User centaurUser = beanFactory.getBean("centaur-user", User.class);
+        System.out.println("centaur-user 与 user Bean是否相同：" + (user == centaurUser));
+    }
+}
