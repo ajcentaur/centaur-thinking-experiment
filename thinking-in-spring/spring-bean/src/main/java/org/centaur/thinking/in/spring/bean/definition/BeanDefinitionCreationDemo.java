097c678c4ecad3972346956ff22b0b5e96a91d08
Centaur <425540677@qq.com>
2023-08-23 22:35:49 +0800

    commit

--- thinking-in-spring/spring-bean/src/main/java/org/centaur/thinking/in/spring/bean/definition/BeanDefinitionCreationDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/org/centaur/thinking/in/spring/bean/definition/BeanDefinitionCreationDemo.java
@@ -1,6 +1,10 @@
 package org.centaur.thinking.in.spring.bean.definition;
 
+import org.centaur.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 
 /**
  * {@link org.springframework.beans.factory.config.BeanDefinition} 构建示例
@@ -12,7 +16,24 @@ public class BeanDefinitionCreationDemo {
 
     public static void main(String[] args) {
         //1. 通过BeanDefinitionBuilder 构建
-        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition();
+        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(User.class);
+        // 设置属性
+        builder.addPropertyValue("id",22);
+        builder.addPropertyValue("name", "人马怪");
+        // 获取 BeanDefinition 实例
+        BeanDefinition definition = builder.getBeanDefinition();
+
+        // 2. 通过AbstractBeanDefinition 以及派生类构建
+        GenericBeanDefinition genericBeanDefinition = new GenericBeanDefinition();
+        // 设置Bean类型
+        genericBeanDefinition.setBeanClass(User.class);
+        //设置属性
+        MutablePropertyValues mutablePropertyValues = new MutablePropertyValues();
+        mutablePropertyValues
+                .add("id", 24)
+                .add("name", "人马怪2");
+        genericBeanDefinition.setPropertyValues(mutablePropertyValues);
+
     }
 
 }
