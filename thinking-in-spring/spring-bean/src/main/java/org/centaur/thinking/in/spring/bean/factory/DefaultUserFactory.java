aea49fb06db8a6c8c99ff91bb7bd2f495ace2af0
Centaur <425540677@qq.com>
2023-08-27 23:56:51 +0800

    Spring Bean销毁方式

--- thinking-in-spring/spring-bean/src/main/java/org/centaur/thinking/in/spring/bean/factory/DefaultUserFactory.java
+++ thinking-in-spring/spring-bean/src/main/java/org/centaur/thinking/in/spring/bean/factory/DefaultUserFactory.java
@@ -1,8 +1,10 @@
 package org.centaur.thinking.in.spring.bean.factory;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * @author Centaur
@@ -10,19 +12,34 @@ import javax.annotation.PostConstruct;
  * @description: TODO
  * @date 2023/8/26 20:08
  */
-public class DefaultUserFactory implements UserFactory, InitializingBean {
+public class DefaultUserFactory implements UserFactory, InitializingBean, DisposableBean {
 
     @PostConstruct
     public void init(){
-        System.out.println("@PostConstruct UserFactory正在初始化中。。。。");
-    }
-
-    public void initAnnotationBean(){
-        System.out.println("AnnotationBean UserFactory正在初始化中。。。。");
+        System.out.println("@PostConstruct UserFactory正在初始化中...");
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println("InitializingBean#afterPropertiesSet() UserFactory正在初始化中。。。。");
+        System.out.println("InitializingBean#afterPropertiesSet() UserFactory正在初始化中...");
+    }
+
+    public void initAnnotationBean(){
+        System.out.println("自定义初始化方法 UserFactory正在初始化中...");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("@PreDestroy UserFactory正在销毁中...");
+    }
+
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean#destroyt() UserFactory正在销毁中...");
+    }
+
+    public void destroyUserFactory(){
+        System.out.println("自定义销毁方法 UserFactory正在销毁中...");
     }
 }
--- thinking-in-spring/spring-bean/src/main/java/org/centaur/thinking/in/spring/bean/initializing/BeanInitializingDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/org/centaur/thinking/in/spring/bean/initializing/BeanInitializingDemo.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 @Configuration
 public class BeanInitializingDemo {
+
     public static void main(String[] args) {
         //1. 使用xml配置文件中的init-method实现
 //        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-initializing-context.xml");
@@ -23,18 +25,25 @@ public class BeanInitializingDemo {
         //2. 使用
         // 声明注解方式的ApplicationContext
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        // 注册配置类
         applicationContext.register(BeanInitializingDemo.class);
+        // 启动Spring应用上下文
         applicationContext.refresh();
+        System.out.println("Spring应用上下文启动...");
         // 2. 使用JAVA API中的@PostConstruct注解
         // 3. 使用@Bean注解中的initMethod
         // 4. 使用InitializingBean#afterPropertiesSet()
         // 5. 使用AbstractBeanDefinition#setInitMethodName(String initMethodName),XML的init-method和@Bean注解中的initMethod是映射到这个方法进行的实现
         UserFactory userFactory = applicationContext.getBean(UserFactory.class);
+        System.out.println("Spring应用上下文准备关闭...");
+        // 关闭Spring容器
         applicationContext.close();
     }
 
-    @Bean(initMethod = "initAnnotationBean")
+    @Bean(initMethod = "initAnnotationBean", destroyMethod = "destroyUserFactory")
+    @Lazy(value = false) //也可以使用xml中的init-lazy
     public UserFactory userFactory(){
         return new DefaultUserFactory();
     }
+
 }
